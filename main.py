ab0c4e04f37d626914faf6b781dbb419adb518fb
alexandrgurin25 <alexandrgurin04@gmail.com>
2023-03-17 13:45:39 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,44 @@
+import os
+import sys
+import telebot
+import openai
+
+
+# указываем ключ из личного кабинета openai
+openai.api_key = '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'
+messages = []
+
+# Создаем экземпляр бота
+bot = telebot.TeleBot('6007451380:AAEjLTNJCUOeeAHLYP93CcfP-6l6EVOC-OI')
+
+# Функция, обрабатывающая команду /start
+@bot.message_handler(commands=["start"])
+def start(m, res=False):
+    bot.send_message(m.chat.id, 'Я на связи. Напиши мне что-нибудь )')
+
+@bot.message_handler(content_types=["photo"])
+def handle_text(tg_message):
+    bot.send_message(tg_message.chat.id, 'Я думаю, что на картинке что-то очень важное, но открывать специально не буду:)')
+
+# Получение сообщений от юзера
+@bot.message_handler(content_types=["text"])
+def handle_text(tg_message):
+    try:
+        user_id = tg_message.chat.id
+        user_name = tg_message.from_user.username
+        message = tg_message.text
+        messages.append({"role": "user", "content": message})
+        chat = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages = messages)
+        reply = chat.choices[0].message.content
+        bot.send_message(tg_message.chat.id, reply)
+        messages.append({"role":"assistant", "content": reply})
+        #print(f'@{user_name}({user_id})->'+message)
+        #print('Bot->'+reply)
+    except Exception as e:
+        bot.send_message(tg_message.chat.id, "Пожалуйста, введите свой запрос еще раз!")
+        #print(e)
+        # при возникновении исключения запускаем программу заново
+        os.execv(sys.executable, [sys.executable] + sys.argv)
+
+# Запускаем бота
+bot.polling(none_stop=True, interval=0)
