f8532ac7aca8a4d280bb332f5aa604563128e8cf
Александр Гурин <alexandrgurin04@gmail.com>
2023-03-31 18:11:48 +0300

    new commit

--- api_key.py
+++ api_key.py
@@ -3,4 +3,8 @@ import openai
 # указываем ключ из личного кабинета openai
 openai.api_key = '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'
 # Создаем экземпляр бота
+<<<<<<< Updated upstream
 bot = telebot.TeleBot('6007451380:AAHT5IeSV9j0Wkbn0m1U_RxnQrmziaeAHfQ')
+=======
+bot = telebot.TeleBot('6007451380:AAHT5IeSV9j0Wkbn0m1U_RxnQrmziaeAHfQ')
+>>>>>>> Stashed changes
