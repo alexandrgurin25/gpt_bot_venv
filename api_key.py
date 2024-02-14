d8c35439c1528113eb1c7489f10a86dcf46c21ad
alex <alexandrgurin04@gmail.com>
2024-02-14 13:29:27 +0300

    ignor api_key.py

--- api_key.py
+++
@@ -1,7 +0,0 @@
-import telebot
-import openai
-# указываем ключ из личного кабинета openai
-openai.api_key = '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'
-# Создаем экземпляр бота
-bot = telebot.TeleBot('6007451380:AAHT5IeSV9j0Wkbn0m1U_RxnQrmziaeAHfQ')
-
